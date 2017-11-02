7dbef2a2954e1d8ac3589bd070e562470f62c6c3
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-11-02 22:48:39 +0100

    Adding CRTP cast operator

--- include/Metaprogramming.hpp
+++ include/Metaprogramming.hpp
@@ -5,6 +5,7 @@
 ///
 /// \brief Header file for the inclusion of all low level metaprogramming
 
+#include <metaprogramming/CRTP.hpp>
 #include <metaprogramming/GroupMacroStatements.hpp>
 #include <metaprogramming/SwallowSemicolon.hpp>
 #include <metaprogramming/TupleTypeCat.hpp>
---
+++ include/metaprogramming/CRTP.hpp
@@ -0,0 +1,24 @@
+#ifndef _CRTP_HPP
+#define _CRTP_HPP
+
+/// \file CRTP.hpp
+///
+/// \brief Header file to support Curiously Recurring Template Pattern
+
+/// Perform the cast to base class needed for CRTP
+///
+/// Overload the ~ operator to implement cast to the basic class of a
+/// CRTP class
+///
+#define DEFINE_CRTP_CAST_OPERATOR(CLASS)		\
+  CLASS& operator~()					\
+  {							\
+    return *static_cast<CLASS*>(this);			\
+  }							\
+  const CLASS& operator~() const			\
+  {							\
+    return *static_cast<const CLASS*>(this);		\
+  }							\
+  CLASS_EAT_SEMICOLON
+
+#endif
