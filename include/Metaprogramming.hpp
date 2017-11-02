4733c26f753bf7f952be51a04f9579bc9dfc4141
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-11-02 11:23:47 +0100

    Adding Metaprogramming

--- include/Metaprogramming.hpp
+++ include/Metaprogramming.hpp
@@ -1,8 +1,10 @@
 #ifndef _METAPROGRAMMING_HPP
 #define _METAPROGRAMMING_HPP
 
-/// \file MetaProgramming.hpp
+/// \file Metaprogramming.hpp
 ///
 /// \brief Header file for the inclusion of all low level metaprogramming
+
+#include <metaprogramming/SwallowSemicolon.hpp>
 
 #endif
---
+++ include/metaprogramming/SwallowSemicolon.hpp
@@ -0,0 +1,31 @@
+#ifndef _SWALLOWSEMICOLON_HPP
+#define _SWALLOWSEMICOLON_HPP
+
+/// \file SwallowSemicolon.hpp
+///
+/// \brief Header file for the macros to swallow a semicolon after a macro
+
+
+/// Define an anonymous empty class to eat a dangling semicolon
+///
+/// Example:
+///
+/// \code
+/// #define DEFINE_MEMBER_WITH_GETTER(TYPE,NAME) \
+///     TYPE NAME;				 \
+///     TYPE get_ ## NAME()			 \
+///     {					 \
+///        return NAME;				 \
+///     }					 \
+///     SWALLOW_SEMICOLON
+///
+/// class test
+/// {
+///    DEFINE_MEMBER_WITH_GETTER(int,a);
+/// };
+/// \endcode
+///
+#define SWALLOW_SEMICOLON			\
+  class {}
+
+#endif
