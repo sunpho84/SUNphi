94c5876c178c52c6abfcdc1f8daf1835fa7c3921
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-11-05 00:50:38 +0100

    Adding Tens.hpp and TensComp.hpp

---
+++ include/Tens.hpp
@@ -0,0 +1,10 @@
+#ifndef _TENS_HPP
+#define _TENS_HPP
+
+/// \file Tens.hpp
+///
+/// \brief Header file for the inclusion of all tensor description
+
+#include <tens/TensComp.hpp>
+
+#endif
---
+++ include/tens/TensComp.hpp
@@ -0,0 +1,61 @@
+#ifndef _TENSCOMP_HPP
+#define _TENSCOMP_HPP
+
+/// \file TensComp.hpp
+///
+/// \brief Defines the Tensor Components
+
+/// TODO: adding a isTensComp check, inheriting
+
+namespace SUNphi
+{
+  /// Static version of the TensCompT
+  ///
+  /// A tensor component with a compile-time known value of max index
+  ///
+  template <class T,int Size>
+  struct TensCompT
+  {
+    static constexpr bool isDynamic=false; ///< Keep track of the fact that the component max value is not dynamic
+    static constexpr int  size=Size;       ///< Size of the tensor component (max index value)
+  };
+  
+  /// Constant used to instantiate a Dynamic-size TensCompT
+  ///
+  constexpr int DYNAMIC=-1;
+  
+  /// Dynamic version of the TensCompT
+  ///
+  /// A tensor component with a run-time known value of max index
+  ///
+  template <class T>
+  struct TensCompT<T,DYNAMIC>
+  {
+    static constexpr bool isDynamic=true; ///< Keep track of the fact that the component max value is dynamic
+    const int &size;                       ///< Size of the tensor (reference to external size)
+    
+    TensCompT(const int &size) : size(size) {} ///< Default constructor taking the size as a constant reference
+  };
+  
+  /// Counts the number of dynamic components of a list of tensor types
+  ///
+  /// Multiple parameter case, calling iteratively itself
+  ///
+  template <typename T,typename...comps>
+  constexpr int NDynComps=NDynComps<T>+NDynComps<comps...>;
+  
+  /// Counts the number of dynamic components of a list of tensor types
+  ///
+  /// Terminator for the single type case
+  ///
+  template <class T>
+  constexpr int NDynComps<T> =T::isDynamic;
+  
+  // template<class T>
+  // using is_dynamic_comp_t=std::conditional_t<T::is_dynamic,std::true_type,std::false_type>;
+  
+  // template<class T>
+  // using is_static_comp_t=std::conditional_t<not T::is_dynamic,std::true_type,std::false_type>;
+}
+
+#endif
