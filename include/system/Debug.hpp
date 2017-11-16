81194a8902fb35ae47d9ad69a4c5e4a0f2c86235
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-11-17 00:13:50 +0100

    Including a simple Print

--- include/system/Debug.hpp
+++ include/system/Debug.hpp
@@ -5,17 +5,33 @@
 ///
 /// \brief Header file for debugging routines
 
-#include <cstdarg>
 #include <cstdio>
 #include <iostream>
 
 /// Macro to make the crash being more explicit
 ///
 #define CRASH(...)							\
-  SUNPhi::internalCrash(__LINE__,__FILE__,__PRETTY_FUNCTION__,__VA_ARGS__)
+  SUNphi::internalCrash(__LINE__,__FILE__,__PRETTY_FUNCTION__,__VA_ARGS__)
 
 namespace SUNphi
 {
+  /// Variadic print to a stream
+  ///
+  std::ostream& Print(std::ostream& out)
+  {
+    return out;
+  }
+  
+  /// Variadic print to a stream
+  ///
+  template <class Head,class...Tail>
+  std::ostream& Print(std::ostream& out,const Head& head,const Tail&...tail)
+  {
+    out<<head;
+    if(sizeof...(tail)) out<<" ";
+    return Print(out,tail...);
+  }
+  
   /// Crash with a detailed message
   ///
   /// At the moment print an error message and gets out, to be
@@ -23,12 +39,9 @@ namespace SUNphi
   /// robust variadic-template one
   ///
   template <class...Args>
-  void internalCrash(int line,const char *file,const char *func,const char *format,const Args&&...args)
+  void internalCrash(int line,const char *file,const char *func,const Args&...args)
   {
-    char buffer[1024];
-    vsnprintf(buffer,1024,format,args...);
-    
-    std::cerr<<"ERROR in function "<<func<<" at line "<<line<<" of file "<<file<<": \""<<buffer<<"\""<<std::endl;
+    Print(std::cerr,"ERROR in function ",func," at line ",line," of file ",file,": \"",args...,"\"\n");
     //print_backtrace_list();
     exit(1);
   }
