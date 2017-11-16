8c8b04b620197a72f2aa2dd80bb430a191390da1
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-11-16 22:19:16 +0100

    Simplified the implementation of TensComp

--- include/tens/TensComp.hpp
+++ include/tens/TensComp.hpp
@@ -13,71 +13,22 @@ namespace SUNphi
   ///
   DEFINE_BASE_TYPE(TensComp);
   
-  /// Static version of the TensCompT
-  ///
-  /// A tensor component with a compile-time known value of max index
-  ///
-  template <class T,int Size>
-  struct TensCompT : public BaseTensComp
-  {
-    static constexpr bool isDynamic=false; ///< Keep track of the fact that the component max value is not dynamic
-    static constexpr int  size=Size;       ///< Size of the tensor component (max index value)
-  };
-  
-  /// Constant used to instantiate a Dynamic-size TensCompT
+  /// Constant used to instantiate a Dynamic-size TensComp
   ///
   constexpr int DYNAMIC=-1;
   
-  /// Dynamic version of the TensCompT
+  /// Tensor Component
   ///
-  /// A tensor component with a run-time known value of max index
+  /// A tensor component is defined by an internal type and the
+  /// associated size, which can be known at compile-time or can be
+  /// made \c DYNAMIC, to be specified at runtime in the storage class
   ///
-  template <class T>
-  struct TensCompT<T,DYNAMIC> : public BaseTensComp
+  template <class T,int Size=DYNAMIC>
+  struct TensComp : public BaseTensComp
   {
-    static constexpr bool isDynamic=true; ///< Keep track of the fact that the component max value is dynamic
-    const int &size;                       ///< Size of the tensor (reference to external size)
-    
-    TensCompT(const int &size) : size(size) {} ///< Default constructor taking the size as a constant reference
+    using type=T;                         ///< Mapped type
+    static constexpr int size=Size;       ///< Size of the tensor component (max index value)
   };
-  
-  namespace Impl
-  {
-    /// Counts the number of dynamic components of a list of tensor types
-    ///
-    /// Internal implementation, raising a static-assert if not
-    /// inheriting from BaseTensComp.
-    ///
-    template <class T,class=ConstraintInheritT<BaseTensComp,T>>
-    constexpr int NDynComps()
-    {
-      return T::isDynamic;
-    }
-  }
-  
-  /// Counts the number of dynamic components of a list of tensor types
-  ///
-  /// Multiple parameter case, calling iteratively itself
-  ///
-  template <typename T,typename...comps>
-  constexpr int NDynComps=NDynComps<T>+NDynComps<comps...>;
-  
-  /// Counts the number of dynamic components of a list of tensor types
-  ///
-  /// Terminator for the single type case
-  ///
-  template <class T>
-  constexpr int NDynComps<T> =Impl::NDynComps<T>();
-  
-  /// Checks wheter T is a dynamic tens comp
-  ///
-  template<class T>
-  constexpr bool IsDynamicTensComp=Conditional<T::isDynamic,TrueType,FalseType>::value;
-  
-  /// Checks wheter T is a non-dynamic tens comp
-  ///
-  template<class T>
-  constexpr bool IsStaticComp=Conditional<not T::isDynamic,TrueType,FalseType>::value;
 }
 
 #endif
