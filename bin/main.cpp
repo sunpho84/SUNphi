a7e92e300f49106a4eb18020099e9ce8cf47266d
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-12-17 00:22:52 +0100

    Rewritten the nested binder

--- bin/main.cpp
+++ bin/main.cpp
@@ -34,67 +34,67 @@ struct Spin : public TensComp<SpinKind,NSPIN>
 };
 
 template <typename Tk,class Tg>
-auto bind(Tk& ref,const Tg&,const int id)
+auto bind(Tk&& ref,const Tg&,const int id)
 {
-  return Binder<Tk,Tg>(ref,id);
+  return Binder<Tk,Tg>(std::forward<Tk>(ref),id);
 }
 
-template <typename Nested,typename NestedTg,class Tg>
-auto bind(Binder<Nested,NestedTg>& ref,const Tg&,const int id)
-{
-  using NestedTk=typename Nested::TK;
-  using Tk=Binder<Nested,NestedTg>;
-  using NestedTypes=typename NestedTk::Types;
-  constexpr int nestedNestedTgPos=posOfType<NestedTg,NestedTypes>;
-  constexpr int nestedTgPos=posOfType<Tg,NestedTypes>;
-  const int nestedId=ref.id;
+// template <typename Nested,typename NestedTg,class Tg>
+// auto bind(Binder<Nested,NestedTg>& ref,const Tg&,const int id)
+// {
+//   using NestedTk=typename Nested::TK;
+//   using Tk=Binder<Nested,NestedTg>;
+//   using NestedTypes=typename NestedTk::Types;
+//   constexpr int nestedNestedTgPos=posOfType<NestedTg,NestedTypes>;
+//   constexpr int nestedTgPos=posOfType<Tg,NestedTypes>;
+//   const int nestedId=ref.id;
   
-  return (nestedTgPos>nestedNestedTgPos)?
-    Binder<Tk,Tg>(ref,id):
-    bind(bind(ref.ref,Tg{},id),NestedTg{},nestedId);
-}
+//   return (nestedTgPos>nestedNestedTgPos)?
+//     Binder<Tk,Tg>(ref,id):
+//     bind(bind(ref.ref,Tg{},id),NestedTg{},nestedId);
+// }
 
 
 
-template <typename T,class Tg>
-auto bind(T&& ref,const int id)
+// template <typename T,class Tg>
+// auto bind(T&& ref,const int id)
+// {
+//   return Binder<T,Tg>(std::forward<T>(ref),id);
+// }
+
+template <typename InNested,                                          // Type referred from the nested bounder
+	  typename InNestedTg,                                        // Type got by the nested bounder
+	  typename Tg>                                                // Type to get
+auto bind(Binder<InNested,InNestedTg>&& ref,const Tg&,const int id)
 {
-  return Binder<T,Tg>(std::forward<T>(ref),id);
-}
-
-namespace SUNphi
-{
-  namespace Impl
-{
-  template <typename Nested,                                        // Type referred from the nested bounder
-	    typename NestedTg,                                      // Type got by the nested bounder
-	    class Tg,                                               // Type to get
-	    class B=Binder<Nested,NestedTg>,                        // Type to be bound
-	    class NestedTK=typename Nested::TK,                     // Tensor Kind of nested bounder
-	    class NestedTypes=typename NestedTK::Types,             // Types of the Tensor Kind of nested bounder
-	    int NestedNestedTgPos=posOfType<NestedTg,NestedTypes>,  // Position inside the nested reference of the type got by the nested bounder
-	    int NestedTgPos=posOfType<Tg,NestedTypes>,              // Position inside the nested reference of the type to get
-	    int Swap=(NestedTgPos>NestedNestedTgPos)>
-  struct NestedBinder
-  {
-    typedef SUNphi::Conditional<Swap,Tg,NestedTg> intTg;
-    typedef SUNphi::Conditional<Swap,NestedTg,Tg> extTg;
-    typedef Binder<Nested,intTg> intBinder;
-    typedef Binder<intBinder,extTg> extBinder;
-    
-    extBinder binder;
-    
-    NestedBinder(Binder<Nested,NestedTg>&& ref,const int id) : binder(intBinder(ref.ref,(Swap?id:ref.id)),(Swap?ref.id:id)) {}
-  };
-  }
-}
-
-template <typename Nested,
-	  typename NestedTg,
-	  class Tg>
-auto bind(Binder<Nested,NestedTg>&& ref,const Tg&,const int id)
-{
-  return Impl::NestedBinder<Nested,NestedTg,Tg>(std::forward<Binder<Nested,NestedTg>>(ref),id).binder;
+  /// Tensor Kind of input nested binder
+  using InNestedTK=typename RemoveReference<InNested>::TK;
+  /// Types of the Tensor Kind of nested bounder
+  using NestedTypes=typename InNestedTK::Types;
+  /// Position inside the nested reference of the type got by the nested bounder
+  constexpr int InNestedNestedTgPos=posOfType<InNestedTg,NestedTypes>;
+  /// Position inside the nested reference of the type to get
+  static constexpr int NestedTgPos=posOfType<Tg,NestedTypes>;
+  /// Keep note of whether we need to swap
+  constexpr bool swap=(NestedTgPos>InNestedNestedTgPos);
+  /// Type got by the output nested binder
+  using OutNestedTg=Conditional<swap,Tg,InNestedTg>;
+  /// Type got by the output binder
+  using OutTg=Conditional<swap,InNestedTg,Tg>;
+  /// Nested component
+  const int nestedId=ref.id;
+  /// Out external component
+  const int outNestedId=(swap?id:nestedId);
+  /// Out component
+  const int outId=(swap?nestedId:id);
+  /// Output Nested binder
+  auto outNestedBinder=bind(std::forward<InNested>(ref.ref),OutNestedTg{},outNestedId);
+  /// Type of the output nested binder
+  using OutNestedBinder=decltype(outNestedBinder);
+  
+  // cout<<"Constructing a nested binder for type "<<Tg::name()<<", internal binder gets: "<<InNestedTg::name()<<", swap: "<<swap<<endl;
+  // cout<<"OutTg: "<<OutTg::name()<<" "<<endl;
+  return Binder<OutNestedBinder,OutTg>(std::move(outNestedBinder),outId);
 }
 
 template <class T>
@@ -117,7 +117,7 @@ using MyTK=TensKind<Color,Spin>;
 class MyTens
 {
   using TS=TensStor<MyTK,double>;
-
+  
 public:
   
   typedef MyTK TK;
@@ -129,7 +129,7 @@ public:
   friend double& eval(MyTens& t,const Comps&...comps)
   {
     static_assert(sizeof...(comps)==TK::nTypes,"The number of arguments must be the same of the number");
-    
+    //print(cout,"Components: ",comps...,"\n");
     return eval(t.v,std::forward<const Comps>(comps)...);
   }
 };
@@ -137,13 +137,20 @@ public:
 int main()
 {
   MyTens cicc;
-  index<MyTK>(3.0,2);
   
-  auto binder1=spin(color(cicc,1),2);
-  auto binder2=color(spin(cicc,2),1);
+  for(int ic=0;ic<3;ic++)
+    for(int id=0;id<4;id++)
+      {
+	double &ref=eval(color(spin(cicc,id),ic));
+	printf("%lld %lld\n",(long long int)cicc.v._v,(long long int)&ref);
+	ref=3.141592352352;
+      }
   
-  eval(binder1)=8.0;
-  printf("%lg\n",eval(binder2));
+  //auto binder1=spin(color(cicc,1),3);
+    //auto binder2=color(spin(cicc,2),1);
+  
+  // eval(binder1)=8.0;
+  //printf("ANNA %lg\n",eval(binder1));
   
 // #ifdef WITH
 //   auto binder1=spin(color(cicc,1),2);
