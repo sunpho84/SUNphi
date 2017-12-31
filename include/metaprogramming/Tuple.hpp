08607d22bd391e31464223916746937b74184f65
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-12-31 14:04:50 +0100

    Removed two interface to tuple, making confusion

--- include/metaprogramming/Tuple.hpp
+++ include/metaprogramming/Tuple.hpp
@@ -256,46 +256,27 @@ namespace SUNphi
   
   /////////////////////////////////////////////////////////////////
   
-  /// Gets element I from the tuple.
-  ///
-  /// Directly aliasing the std library. L-value case.
-  /// \return A reference to the I tuple element.
-  template<std::size_t I,class...Types>
-  constexpr std::tuple_element_t<I,Tuple<Types...>>&
-  get(Tuple<Types...>& t)
-  {
-    return std::get<I>(t);
-  }
-  
   /// Gets an element from the tuple.
   ///
   /// Directly aliasing the std library. R-value case
   /// \return A move-reference to the I tuple element.
-  template<std::size_t I,class...Types>
-  constexpr std::tuple_element_t<I,Tuple<Types...>>&&
-  get(Tuple<Types...>&& t)
+  template<std::size_t I,
+	   class T>
+  constexpr std::tuple_element_t<I,T>&&
+  get(T&& t)
   {
-    return std::get<I>(t);
-  }
-  
-  /// Gets the element of type T from the tuple.
-  ///
-  /// Directly aliasing the std library. L-value case.
-  /// \return A reference to the T tuple type (if present).
-  template<class T,class...Types>
-  constexpr T& get(Tuple<Types...>& t)
-  {
-    return std::get<T>(t);
+    return std::get<I>(std::forward<T>(t));
   }
   
   /// Gets the element of type T from the tuple.
   ///
   /// Directly aliasing the std library. R-value case.
   /// \return A move-reference to the T tuple type (if present).
-  template<class T,class...Types>
-  constexpr T&& get(Tuple<Types...>&& t)
+  template<class Tg,
+	   class T>
+  constexpr Tg&& get(T&& t)
   {
-    return std::get<T>(t);
+    return std::get<Tg>(std::forward<T>(t));
   }
   
   /////////////////////////////////////////////////////////////////
