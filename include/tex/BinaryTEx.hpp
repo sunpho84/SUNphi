97180b4504160c91a8c8428346f6bb5417dde703
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-01-09 17:24:34 +0100

    Added BinaryTEx builder

--- include/tex/BinaryTEx.hpp
+++ include/tex/BinaryTEx.hpp
@@ -37,6 +37,39 @@ namespace SUNphi
 #define PROVIDE_BINARY_TEX_REFS			\
   PROVIDE_TEX_REF(1);				\
   PROVIDE_TEX_REF(2)
+  
+  /// Create a simple builder with a name and a BINARY_TEX returned type
+#define SIMPLE_BINARY_TEX_BUILDER(BUILDER,    /*!< Name of builder fun           */ \
+				  BINARY_TEX) /*!< Name of the BinaryTEx to build */ \
+  /*! Simple BINARY_TEX builder called BUILDER */			\
+  /*!                                          */			\
+  /*! Plain BINARY_TEX getting a pair of plain TEx                    */ \
+  template <typename T1, 	    /* Type of the first TEx to get   */ \
+	    typename T2, 	    /* Type of the second TEx to get  */ \
+	    SFINAE_WORSEN_DEFAULT_VERSION_TEMPLATE_PARS>		\
+  BINARY_TEX<T1,T2> BUILDER(T1&& tex1,   /*!< First TEx to act upon  */	\
+			    T2&& tex2,   /*!< Second TEx to act upon */	\
+			    SFINAE_WORSEN_DEFAULT_VERSION_ARGS)		\
+  {									\
+    SFINAE_WORSEN_DEFAULT_VERSION_ARGS_CHECK;				\
+  									\
+    return BINARY_TEX<T1,T2>(forw<T1>(tex1),forw<T2>(tex2));		\
+  }									\
+  SWALLOW_SEMICOLON_AT_GLOBAL_SCOPE
+  
+  /// Set aliasing according to the isAliasing of references 1 and 2
+  /// \todo enforce cehck only with TensClass
+#define FORWARD_IS_ALIASING_TO_REF_1_2			\
+  /*! Forward aliasing check to the references */	\
+  template <typename Tref>				\
+  bool isAliasing(const Tref& alias) const		\
+  {							\
+    return						\
+      ref1.isAliasing(alias) or				\
+      ref2.isAliasing(alias);				\
+  }							\
+  SWALLOW_SEMICOLON_AT_CLASS_SCOPE
+  
   /// Set the assignability according to the references 1 and 2
 #define ASSIGNABLE_ACCORDING_TO_REF_1_2					\
   IS_ASSIGNABLE_ATTRIBUTE(/*! This TEx can be assigned according to the references 1 and 2 */, \
