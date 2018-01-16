cd56780a05f8e11f57d468f454b8f166550219d8
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-01-16 16:25:22 +0100

    Removed grouper

--- include/Metaprogramming.hpp
+++ include/Metaprogramming.hpp
@@ -6,7 +6,6 @@
 /// \brief Header file for the inclusion of all low level metaprogramming
 
 #include <metaprogramming/CRTP.hpp>
-#include <metaprogramming/GroupMacroStatements.hpp>
 #include <metaprogramming/IntSeq.hpp>
 #include <metaprogramming/SwallowSemicolon.hpp>
 #include <metaprogramming/Tuple.hpp>
