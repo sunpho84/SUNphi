9c34d16e48f234bd8343f98f51e059e8b2bd6dcb
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-01-16 22:51:16 +0100

    Adding namespace SUNphi

--- include/metaprogramming/CRTP.hpp
+++ include/metaprogramming/CRTP.hpp
@@ -7,22 +7,25 @@
 
 #include <metaprogramming/SwallowSemicolon.hpp>
 
-/// Perform the cast to base class needed for CRTP
-///
-/// Overload the ~ operator to implement cast to the basic class of a
-/// CRTP class
+namespace SUNphi
+{
+  /// Perform the cast to base class needed for CRTP
+  ///
+  /// Overload the ~ operator to implement cast to the basic class of a
+  /// CRTP class
 #define PROVIDE_CRTP_CAST_OPERATOR(CLASS)		\
   /*! Cast operator to class \c CLASS*/			\
-CLASS& operator~()					\
+  CLASS& operator~()					\
   {							\
     return *static_cast<CLASS*>(this);			\
   }							\
-							\
- /*! Constant cast operator to class \c CLASS */	\
-const CLASS& operator~() const				\
+  							\
+  /*! Constant cast operator to class \c CLASS */	\
+  const CLASS& operator~() const			\
   {							\
     return *static_cast<const CLASS*>(this);		\
   }							\
- SWALLOW_SEMICOLON_AT_CLASS_SCOPE
+  SWALLOW_SEMICOLON_AT_CLASS_SCOPE
+}
 
 #endif
