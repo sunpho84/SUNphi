19b07dbe31178ef191fb5a4422d3ed8216e09e82
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-09-14 18:42:40 +0200

    Uminuser built out of uminus

--- include/smet/UnaryMinus.hpp
+++ include/smet/UnaryMinus.hpp
@@ -5,8 +5,79 @@
 ///
 /// \brief Header file for the definition of -smet
 
+#include <tens/TensKind.hpp>
+#include <physics/Spin.hpp>
+#include <smet/UnarySmET.hpp>
+
 namespace SUNphi
 {
+  // Base type to qualify as UMinuser
+  DEFINE_BASE_TYPE(UMinuser);
+  
+  /// Class to take the conjugate of a SmET
+  template <typename _Ref,                                  // Type of the expression to conjugate
+	    typename TK=typename RemoveReference<_Ref>::Tk, // Tens Kind of the conjugated type
+	    typename TK_TYPES=typename TK::types>           // Types of the tensor kind
+  class UMinuser :
+    public BaseUMinuser,                                // Inherit from BaseUMinuser to detect in expression
+    public UnarySmET<UMinuser<_Ref>>,                   // Inherit from UnarySmET
+    public ConstrainIsSmET<_Ref>,                     // Constrain _Ref to be a SmET
+    public ConstrainIsTensKind<TK>                    // Constrain type TK to be a TensKind
+  {
+    
+  public:
+    
+    PROVIDE_UNARY_SMET_REF;
+    
+    // Attributes
+    NOT_STORING;
+    NOT_ASSIGNABLE;
+    FORWARD_IS_ALIASING_TO_REF;
+    
+    /// TensorKind of the bound expression
+    using Tk=TK;
+    
+    // As mergeable as its ref
+    MERGEABLE_ACCORDING_TO_REF;
+    
+    PROVIDE_UNARY_SMET_SIMPLE_GET_MERGED_COMPS_VIEW(UMinuser);
+    
+    SAME_COMP_SIZES_OF_REF;
+    
+    PROVIDE_UNARY_SMET_SIMPLE_CREATOR(UMinuser);
+    
+    /// Provides either the const or non-const evaluator
+#define PROVIDE_CONST_OR_NOT_EVALUATOR(QUALIFIER)			\
+    /*! QUALIFIER evaluator for UMinuser                             */ \
+    /*!                                                              */	\
+    template <typename...Args>           /* Type of the arguments    */	\
+    DECLAUTO eval(const Args&...args)    /*!< Components to get      */	\
+      QUALIFIER								\
+    {									\
+      STATIC_ASSERT_ARE_N_TYPES(TK::nTypes,args);			\
+									\
+      /*  Temporary result */						\
+      const auto val=ref.eval(forw<const Args>(args)...);		\
+									\
+      return -val;							\
+    }									\
+    SWALLOW_SEMICOLON_AT_CLASS_SCOPE
+    
+    PROVIDE_CONST_OR_NOT_EVALUATOR(NON_CONST_QUALIF);
+    PROVIDE_CONST_OR_NOT_EVALUATOR(CONST_QUALIF);
+    
+#undef PROVIDE_CONST_OR_NOT_DEFAULT_EVALUATOR
+    
+  };
+  
+  // Check that a test UMinuser is a UnarySmET
+  STATIC_ASSERT_IS_UNARY_SMET(UMinuser<Tens<TensKind<Spin>,double>>);
+  
+  // Build UMinuser from uminus
+  SIMPLE_UNARY_SMET_BUILDER(uminus,UMinuser);
+  
+  // Simplifies uminus(uminus)
+  CANCEL_DUPLICATED_UNARY_SMET_CALL(uminus,UMinuser);
 }
 
 #endif
