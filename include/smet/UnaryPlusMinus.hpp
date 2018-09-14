ec6b57a1f466c8cd996056f435b2fa83f480ab5b
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-09-14 17:47:47 +0200

    Adding include to allow syntax check

--- include/smet/UnaryPlusMinus.hpp
+++ include/smet/UnaryPlusMinus.hpp
@@ -5,13 +5,14 @@
 ///
 /// \brief Header file for the definition of +smet or -smet
 
-#include <metaprogramming/TypeTraits.hpp>
+#include <metaprogramming/SFINAE.hpp>
+#include <metaprogramming/UniversalReferences.hpp>
 #include <smet/BaseSmET.hpp>
 
 namespace SUNphi
 {
   /// Implement +smet: decays into smet itself
-  template <typename T,             // Type of the expression
+  template <typename T,              // Type of the expression
 	    SFINAE_ON_TEMPLATE_ARG(isSmET<T>)>
   DECLAUTO operator+(T&& smet)       ///< Expression
   {
