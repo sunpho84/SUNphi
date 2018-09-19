77890a00c0a111037de5a53be0c3ff0b53610723
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-09-20 01:24:40 +0200

    Adding Pos still to be finished

--- bin/main.cpp
+++ bin/main.cpp
@@ -7,8 +7,55 @@
 using namespace std;
 using namespace SUNphi;
 
+template <int P,
+	  typename T,
+	  template <T A> typename F,
+	  int...Is>
+class _TruePosOfVariadicClass;
+
+template <int P,
+	  typename T,
+	  template <T A> class F>
+class _TruePosOfVariadicClass<P,T,F>
+{
+public:
+  using Out=IntSeq<>;
+};
+
+template <int I>
+class Fun
+{
+public:
+  static constexpr bool res=(I!=0);
+};
+
+template <int P,
+	  typename T,
+	  template <int A> class F,
+	  int I,
+	  int...Oth>
+class _TruePosOfVariadicClass<P,T,F,I,Oth...>
+{
+public:
+  using Out=IntSeqCat<Conditional< F<I>::res,
+				  IntSeq<P>,
+				  IntSeq<>>,
+                      typename _TruePosOfVariadicClass<P+1,T,F,Oth...>::Out>;
+};
+
+template <typename T,
+	  template <T A> class F,
+	  T...I>
+using TruePosOfVariadicClass=typename _TruePosOfVariadicClass<0,T,F,I...>::Out;
+
 int main()
 {
+  using A=TruePosOfVariadicClass<int,Fun, 0,1,0,10>;
+  
+  A aa;
+  //int aaa=aa;
+  
+  
   // Check that conj of a non-complex type object is the same of original type
   {
     using MyTk=TensKind<RwCol,Spin,CnCol>;
@@ -19,12 +66,18 @@ int main()
     STATIC_ASSERT_IS_BASE_OF(MyTens,decltype(conj(c)));
   }
   
-  // Check that UnaryPlus is absorbed
+  
+  
+  //come trovare le componenti non twinnate? crea un IntSeq che dica
+  //se le componenti devono essere prese o no, prendi di questo le
+  //posizioni non nulle, e chiama getIndexed
   {
     using MyTk=TensKind<RwCol,Spin,CnCol>;
     using MyTens=Tens<MyTk,double>;
     
     MyTens c;
+
+    cout<<" "<<posOfType<RwCol,MyTk::Twinned::types><<endl;
     
     STATIC_ASSERT_IS_BASE_OF(MyTens,decltype(+c));
   }
@@ -38,8 +91,6 @@ int main()
     
     STATIC_ASSERT_IS_BASE_OF(MyTens,decltype(-(-c)));
   }
-  
-  
   
   using MyTk=TensKind<RwCol,Spin,Compl,CnCol>;
   using MyTens=Tens<MyTk,double>;
@@ -55,6 +106,11 @@ int main()
 		     ri),
 		irw_col)=
   	  icn_col+NCOL*(0+NCOMPL*(ispin+NSPIN*irw_col));
+
+  cicc.getMaximallyMergedCompsView()=1.0;
+  
+  auto i=imag(conj(cnCol(spin(rwCol(-cicc,0),0),0)));
+  cout<<i<<endl;
   
   cout<<"/////////////////////////////////////////////////////////////////"<<endl;
   
