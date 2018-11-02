848c26beb631dae5c90c01f9ae1696f02478ccbf
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-11-02 16:25:40 +0100

    Adding Nnary to replace binary and generalize it

---
+++ include/smet/MulAdd.hpp
@@ -0,0 +1,100 @@
+#ifndef _MUL_ADD_HPP
+#define _MUL_ADD_HPP
+
+/// \file MulAdd.hpp
+///
+/// \brief Defines a class which take the sum of a \c SmET with the product of two others
+///
+/// For the moment this is just used to provide a testcase to write the generic Nnary 
+
+#include <smet/Reference.hpp>
+#include <smet/NnarySmET.hpp>
+#include <tens/TensKind.hpp>
+#include <tens/TensClass.hpp>
+
+namespace SUNphi
+{
+  // Base type to qualify as MulAdder
+  DEFINE_BASE_TYPE(MulAdder);
+  
+  /// Class to add a \c SmET with the prodcut of two others: \c a+b*c
+  template <typename..._Refs>                    // Reference types
+  class MulAdder :
+    public BaseMulAdder,                         // Inherit from \c BaseMulAdder to detect in expression
+    public NnarySmET<MulAdder<_Refs...>>         // Inherit from \c NnarySmET
+  {
+    static_assert((isSmET<RemRef<_Refs>> && ...),"All types must be SmET");
+    
+  public:
+    
+    /// Position of the elements
+    enum Pos_t{FACT1,
+	       FACT2,
+	       ADDEND};
+    
+    PROVIDE_NNARY_SMET_REFS_AND_CHECK_ARE_N(3);
+    
+    PROVIDE_SIMPLE_NNARY_COMP_SIZE;
+    
+    // Attributes
+    NOT_STORING;
+    FORWARD_IS_ALIASING_TO_REFS;
+    
+    /// TensorKind of the result
+    ///
+    /// \todo Improve, the return type could be decided studying what
+    /// suits best the computational
+    //PROVIDE_TK(BlendTensKinds<TK1,TK2>);
+    
+    /// Fundamental type
+    PROVIDE_FUND(decltype(typename RemRef<Ref<FACT1>>::Fund{}*
+			  typename RemRef<Ref<FACT2>>::Fund{}+
+			  typename RemRef<Ref<ADDEND>>::Fund{}));
+    
+    // PROVIDE_POS_OF_TCS_IN_RES_TK_AND_MERGED_DELIMS_FOR_REF(1);
+    // PROVIDE_POS_OF_TCS_IN_RES_TK_AND_MERGED_DELIMS_FOR_REF(2);
+    
+    // PROVIDE_MERGEABLE_COMPS_ACCORDING_TO_TWO_REFS;
+    
+    // // Returns a component-merged version
+    // PROVIDE_GET_MERGED_COMPS_VIEW(/*! Merge appropriately the two references and returns their sum */,
+    // 				  return
+    // 				  ref1.template getMergedCompsView<MergedDelims1<Is>>()+
+    // 				  ref2.template getMergedCompsView<MergedDelims2<Is>>(););
+    
+    PROVIDE_NNARY_SMET_SIMPLE_CREATOR(MulAdder);
+  };
+  
+  // // Check that a test MulAdder is a BinarySmET
+  // namespace CheckMulAdderIsBinarySmet
+  // {
+  //   /// Tensor comp for test
+  //   using MyTc=
+  //     TensComp<double,1>;
+    
+  //   /// Tensor kind to be tested
+  //   using MyTk=
+  //     TensKind<MyTc>;
+    
+  //   /// Tensor to be tested
+  //   using MyT=
+  //     Tens<MyTk,double>;
+    
+  //   STATIC_ASSERT_IS_BINARY_SMET(MulAdder<MyT,MyT>);
+  // }
+  
+  // Build MulAdder from mulAdd
+  SIMPLE_NNARY_SMET_BUILDER(mulAdd,MulAdder);
+  
+  // /// Implement smet1+smet2
+  // template <typename T1,              // Type of the first expression
+  // 	    typename T2,              // Type of the second expression
+  // 	    SFINAE_ON_TEMPLATE_ARG(isSmET<T1> and isSmET<T2>)>
+  // DECLAUTO operator+(T1&& smet1,      ///< First addend
+  // 		     T2&& smet2)      ///< Second addend
+  // {
+  //   return add(forw<T1>(smet1),forw<T2>(smet2));
+  // }
+}
+
+#endif
---
+++ include/smet/NnarySmET.hpp
@@ -0,0 +1,135 @@
+#ifndef _NNARYSMET_HPP
+#define _NNARYSMET_HPP
+
+/// \file NnarySmET.hpp
+///
+/// \brief Header file defining basic properties of Nnary SmET
+
+#include <ints/IntSeqCat.hpp>
+#include <smet/BaseSmET.hpp>
+#include <tuple/TupleClass.hpp>
+#include <utility/Position.hpp>
+
+namespace SUNphi
+{
+  /// Defines the NnarySmET type traits
+  DEFINE_BASE_TYPE(NnarySmET,: public BaseSmET);
+  
+  // Defines the check for a member type "refs"
+  DEFINE_HAS_MEMBER(refs);
+  
+  /// Defines the check for a Nnary SmET
+#define STATIC_ASSERT_IS_NNARY_SMET(...)		\
+  STATIC_ASSERT_IS_SMET(__VA_ARGS__);			\
+  STATIC_ASSERT_HAS_MEMBER(refs,__VA_ARGS__)
+  
+  /// Nnary SmET
+  template <typename T>  /// Inheriting type
+  struct NnarySmET :
+    public SmET<T>,
+    public BaseNnarySmET
+  {
+    IS_ASSIGNABLE_ATTRIBUTE(/*! This SmET can never be lhs */,false);
+    
+    PROVIDE_CRTP_CAST_OPERATOR(T);
+  };
+  
+  /// Provide the references to the objects
+  ///
+  /// The reference types are contained in a \c Tuple with types
+  /// obtained from template pars
+#define PROVIDE_NNARY_SMET_REFS_AND_CHECK_ARE_N(N)	\
+  /*! Type of the binding references */			\
+  using Refs=						\
+    Tuple<_Refs...>;					\
+  							\
+  /*! Reference to the N object */			\
+  Tuple<Reference<_Refs>...> refs;			\
+  							\
+  /*! Type of I-th \c SmET      */			\
+  template <int I>					\
+  using Ref=						\
+    TupleElementType<I,Tuple<_Refs...>>;		\
+							\
+  /*! Number or \c SmET to act upon */			\
+  static constexpr					\
+  int NSmET=						\
+    N;							\
+							\
+  /* Check that NSmET coincides with the par */		\
+  static_assert(N==sizeof...(_Refs))
+  
+  /// Returns the size of a \c TensComp, with a simple approach
+  ///
+  /// Search the asked \c TensComp in each ref \c TensKind iteratively
+  /// and returns the size in the first found ref
+#define PROVIDE_SIMPLE_NNARY_COMP_SIZE					\
+  /*! Returns the size in the first found ref */			\
+  template <typename TC, /* \c TensComp to search           */		\
+	    int I=0>     /* Integer of the ref to search in */		\
+  int compSize() const							\
+  {									\
+    static_assert(I>=0 and I<NSmET,"Cannot search in negative or larger-than-N ref"); \
+									\
+    /*! \c TensKind of the I-th \c SmET */				\
+    using RefTk=							\
+      typename Ref<I>::Tk;						\
+									\
+    /*! Check if the \c TensKind contains the \c TensComp asked */	\
+    constexpr bool found=						\
+      RefTk::template contains<TC>;					\
+									\
+    /* Returns the size if it's in the I-th Tk */			\
+    if constexpr(found)							\
+		  return get<I>(refs).template compSize<TC>();		\
+    else								\
+      return compSize<TC,I+1>();					\
+  }									\
+  SWALLOW_SEMICOLON_AT_CLASS_SCOPE
+  
+  /////////////////////////////////////////////////////////////////
+  
+  /// Defines a simple creator taking n references
+#define PROVIDE_NNARY_SMET_SIMPLE_CREATOR(NNARY_SMET /*!< Name of the NnarySmET */) \
+  /*! Constructor taking universal reference */				\
+  template <typename...SMETS,						\
+	    typename=EnableIf<((isSame<Unqualified<SMETS>,Unqualified<_Refs>>) && ...)>> \
+  explicit NNARY_SMET(SMETS&&...smets) : refs(forw<SMETS>(smets)...)	\
+  {									\
+  }									\
+  SWALLOW_SEMICOLON_AT_CLASS_SCOPE
+  
+  /////////////////////////////////////////////////////////////////
+  
+  /// Create a simple builder with a name and a NNARY_SMET returned type
+  ///
+  /// \c NnarySmET cannot use SFINAE to worsen default constructors
+  /// (unless packing args in \c Tuple) so the simple builder cannot
+  /// be overloaded. If required, use a custom builder and use if
+  /// constexpr construct inside it to route the manipulations.
+#define SIMPLE_NNARY_SMET_BUILDER(BUILDER,     /*!< Name of builder function        */ \
+				  NNARY_SMET)  /*!< Name of the NnarySmET to build */ \
+  /*! Simple NNARY_SMET builder called BUILDER */			\
+  /*!                                          */			\
+  /*! Plain NNARY_SMET getting n plain SmET    */			\
+  template <typename...Ts> 	   /* Types of the SmET to get       */ \
+  NNARY_SMET<Ts...> BUILDER(Ts&&...smets)    /*!< SmETs to act upon  */ \
+  {									\
+    return NNARY_SMET<Ts...>(forw<Ts>(smets)...);			\
+  }									\
+  SWALLOW_SEMICOLON_AT_GLOBAL_SCOPE
+  
+  /// Set aliasing according to the isAliasing of references
+  /// \todo enforce cehck only with TensClass
+#define FORWARD_IS_ALIASING_TO_REFS			\
+  /*! Forward aliasing check to the references */	\
+  template <typename Tref>				\
+  bool isAliasing(const Tref& alias) const		\
+  {							\
+    CRASH("Fixme");					\
+    return true;					\
+  }							\
+  SWALLOW_SEMICOLON_AT_CLASS_SCOPE
+}
+
+#endif
