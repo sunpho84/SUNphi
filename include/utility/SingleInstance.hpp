1eec6ad7c3cda3af5aada433bc779bb38df5746d
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-01-04 23:23:35 +0100

    Documentatio for SingleInstance

--- include/utility/SingleInstance.hpp
+++ include/utility/SingleInstance.hpp
@@ -4,11 +4,28 @@
 #include <system/Debug.hpp>
 #include <metaprogramming/StaticMemberWithInitializator.hpp>
 
+/// \file SingleInstance.hpp
+///
+/// \brief Defines a simple class, with a counter wich enable only one
+/// instance at the time
+
 namespace SUNphi
 {
-  /// A simple class, with a counter wich enable only one instance at the time
+  /// Single Instance enforcer class
   ///
-  template <typename T> // Type from which inheriting, to distinguish each count
+  /// To enforce a class to be instantiated at most once, inherit this
+  /// class. Example:
+  ///
+  /// \code
+  /// class A :
+  ///   public SingleInstance<A>
+  /// {
+  /// };
+  ///
+  /// A a1; // ok
+  /// A a2; // error!
+  /// \endcode
+  template <typename T> // Type from which to inherit, to distinguish each count
   class SingleInstance
   {
     
