5824d3884395c01fc9b561e02842eb734f05edb9
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-01-21 16:52:18 +0100

    Adding threads

---
+++ include/system/Threads.hpp
@@ -0,0 +1,23 @@
+#ifndef _THREADS_HPP
+#define _THREADS_HPP
+
+/// \file Threads.hpp
+///
+/// \brief Provides a thread pool
+///
+
+#ifdef HAVE_CONFIG_H
+ #include "config.hpp"
+#endif
+
+#ifdef USE_THREADS
+ #include <omp.h>
+#endif
+
+namespace SUNphi
+{
+  
+  
+}
+
+#endif
