965fa77f36da0361deac2e316c126c025fd656dd
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-02-08 13:14:39 +0100

    Adding Random and sitmo

---
+++ include/Random.hpp
@@ -0,0 +1,15 @@
+#ifndef _RANDOM_HPP
+#define _RANDOM_HPP
+
+/// \file Random.hpp
+///
+/// \brief Topical header for random stuff
+///
+
+#include <random/Sitmo.hpp>
+
+namespace SUNphi
+{
+}
+
+#endif
