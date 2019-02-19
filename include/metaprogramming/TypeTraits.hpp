575319c7018a85df361b51569d9fcb980f035897
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-02-20 00:27:01 +0100

    Adding check on operator presence

--- include/metaprogramming/TypeTraits.hpp
+++ include/metaprogramming/TypeTraits.hpp
@@ -585,6 +585,40 @@ namespace SUNphi
   
   /////////////////////////////////////////////////////////////////
   
+#define DEFINE_BINARY_OPERATOR_IMPLEMENTATION_CHECK(CHECK_NAME,		\
+						    STRUCT_NAME,	\
+						    OPERATOR)		\
+  template<typename S,							\
+	 typename T>							\
+struct STRUCT_NAME							\
+{									\
+  /*! Path followed when the operator is implemented */			\
+  template<typename U,							\
+	   typename V>							\
+  static auto test(U*)->decltype(std::declval<U>()			\
+				 OPERATOR				\
+				 std::declval<V>());			\
+									\
+  /*! Default case in which the binary operation cannot be performed */	\
+  template<typename,							\
+	   typename>							\
+  static auto test(...)->std::false_type;				\
+									\
+  /*! Result of the check */						\
+  static constexpr bool res=						\
+    not isSame<std::false_type,decltype(test<S,T>(0))>;			\
+};									\
+  /*! Check that operator OPERATOR is implemented */			\
+  template<typename S,							\
+	   typename T>							\
+  [[ maybe_unused ]]							\
+  constexpr bool CHECK_NAME=						\
+    STRUCT_NAME<S,T>::res
+  
+  DEFINE_BINARY_OPERATOR_IMPLEMENTATION_CHECK(canPrint,CanPrint,<<);
+  
+  /////////////////////////////////////////////////////////////////
+  
   /// Returns the signed version of the passed type
   template <typename T>     // Type to unsign
   using SignedOf=
