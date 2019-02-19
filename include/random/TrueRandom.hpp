6cd75a6df6b4dfca42148e79338551f1300b4ffb
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-02-19 12:57:04 +0100

    Adding the rank

--- include/random/TrueRandom.hpp
+++ include/random/TrueRandom.hpp
@@ -12,15 +12,29 @@
 namespace SUNphi
 {
   /// Class which enables true random numbers
+  ///
+  /// The random numbers are always generated on all nodes, but only
+  /// the value on the given node is returned
   class TrueRandom
     : public std::random_device
   {
+    /// Rank of which the result is returning
+    int returningRank;
+    
   public:
     
+    /// Creates specifying the rank to be used for returning
+    TrueRandom(int returningRank=Mpi::MASTER_RANK)
+      : returningRank(returningRank)
+    {
+    }
+    
+    /// Returns the inner generated value
     auto operator()()
     {
       return
-	mpi.broadcast(static_cast<std::random_device*>(this)->operator()());
+	mpi.broadcast(static_cast<std::random_device*>(this)->operator()(),
+		      returningRank);
     }
   };
 }
