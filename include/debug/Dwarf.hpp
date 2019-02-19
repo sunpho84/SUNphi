adc344910fa3ec07b26bbcbc71f9d93cc1f471ce
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-02-19 14:11:52 +0100

    Adding Dwarf.hpp

---
+++ include/debug/Dwarf.hpp
@@ -0,0 +1,22 @@
+#ifndef _DWARF_HPP
+#define _DWARF_HPP
+
+/// \file Dwarf.hpp
+///
+/// \brief Header file to support debug of the dwarf data
+
+#ifdef HAVE_CONFIG_H
+ #include "config.hpp"
+#endif
+
+#ifdef USE_DWARF
+ #include <libdwarf/dwarf.h>
+#endif
+
+namespace SUNphi
+{
+  
+  
+}
+
+#endif
