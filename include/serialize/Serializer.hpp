a9141d02d5942f89119b29e1921cbb484d902f80
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-03-12 15:17:45 +0100

    Serializing is working

--- include/serialize/Serializer.hpp
+++ include/serialize/Serializer.hpp
@@ -12,65 +12,7 @@
 #include <metaprogramming/SFINAE.hpp>
 #include <serialize/Scalar.hpp>
 
-namespace YAML
-{
-  using namespace SUNphi;
-  
-  /// Serializable scalar conversion to YAML node
-  template <typename T>
-  struct convert<T,
-		 EnableIfIsSerializableScalar<T>>
-  {
-    /// Encode a SerializableScalar
-    static Node encode(const SerializableScalar<T>& rhs) ///< Input
-    {
-      /// Output node
-      Node node;
-      node<<~rhs;
-      
-      return
-	node;
-    }
-    
-    /// Decodes a SerializableScalar
-    static bool decode(const Node& node,             ///< Input node
-		       SerializableScalar<T>& rhs)   ///< Output
-    {
-      node>>rhs.a;
-      
-      return
-	true;
-    }
-  };
-
-  /// Serializable class conversion to YAML node
-  template <typename T>
-  struct convert<T,
-		 EnableIfIsSerializableClass<T,BaseSerializableClass>>
-  {
-    /// Encode a SerializableClass
-    static Node encode(const T& rhs) ///< Input
-    {
-      /// Output node
-      Node node;
-      node<<~rhs;
-      
-      return
-	node;
-    }
-    
-    /// Decodes a SerializableClass
-    static bool decode(const Node& node,             ///< Input node
-		       const T& rhs)                 ///< Output
-    {
-      node>>rhs.a;
-      
-      return
-	true;
-    }
-  };
-}
-
+#include <iostream>
 namespace SUNphi
 {
   /// Convert to string the passed objects
@@ -79,34 +21,61 @@ namespace SUNphi
     /// Node used to store the serialized data
     YAML::Node node;
     
+    /// Writes everything or the default only
+    const bool onlyNonDef{false};
+    
   public:
     
     /// Output a serializable object to a serializer
-    template <typename T>
+    template <typename T,
+	      SFINAE_ON_TEMPLATE_ARG(not (isSerializableClass<T> or isSerializableScalar<T>))>
     friend Serializer& operator<<(Serializer& ser,
 				  const T& t)
     {
-      if constexpr(isSerializableClass<T>)
-	forEach(t.serializableMembers,
-		[&ser](auto s)
-		{
-		  Serializer nested;
-		  nested<<s();
-		  
-		  ser.node=
-		    nested.node;
-		});
-      else
-	if constexpr(isSerializableScalar<T>)
-	  {
-	    Serializer nested;
-	    nested<<t();
-	    
-	    ser.node[t.name]=
-	      nested.node;
-	  }
-	else
-	  ser.node=t;
+            std::cout<<"nothing"<<std::endl;
+
+      ser.node=
+	t;
+      
+      return
+	ser;
+    }
+    
+    /// Output a serializable object to a serializer
+    template <typename T,
+	      SFINAE_ON_TEMPLATE_ARG(isSerializableClass<T>)>
+    friend Serializer& operator<<(Serializer& ser,
+				  const T& t)
+    {
+      forEach(t.serializableMembers,
+	      [&ser](auto s)
+	      {
+		ser<<
+		  s;
+	      });
+      
+      return
+	ser;
+    }
+    
+    /// Output a serializable object to a serializer
+    template <typename T,
+	      SFINAE_ON_TEMPLATE_ARG(isSerializableScalar<T>)>
+    friend Serializer& operator<<(Serializer& ser,
+				  const T& t)
+    {
+      if(not (ser.onlyNonDef and t.isDefault()))
+	{
+	  std::cout<<"scalar "<<t.name<<" "<<ser.onlyNonDef<<" "<<t.isDefault()<<std::endl;
+      
+	  /// Creates the nested serializer
+	  Serializer nested(ser.onlyNonDef);
+	  nested<<
+	    t();
+	  
+	  ser.node[t.name]=
+	    nested.node;
+	}
       
       return
 	ser;
@@ -117,10 +86,17 @@ namespace SUNphi
       const
     {
       YAML::Emitter emitter;
-      emitter<<node;
+      emitter<<
+	node;
       
       return
 	emitter.c_str();
+    }
+    
+    /// Creates a serializer
+    Serializer(const bool& onlyNonDef=false) :
+      onlyNonDef(onlyNonDef)
+    {
     }
   };
 }
