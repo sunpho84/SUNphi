32e80eb81eb0c0aa7c185397f9e9931cb8b1e1c7
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-05-20 19:30:51 +0200

    Added the skeleton of sitmo

---
+++ include/random/Sitmo2.hpp
@@ -0,0 +1,52 @@
+#ifndef SITMO_HPP
+#define SITMO_HPP
+
+/// \file Sitmo.hpp
+///
+/// \brief Implements the SITMO random number generator
+///
+///  Copyright (c) 2012-2016 M.A. (Thijs) van den Berg, http://sitmo.com/
+///
+///  Use, modification and distribution are subject to the MIT Software License.
+///
+///  The MIT License (MIT)
+///  Permission is hereby granted, free of charge, to any person obtaining a copy
+///  of this software and associated documentation files (the "Software"), to deal
+///  in the Software without restriction, including without limitation the rights
+///  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+///  copies of the Software, and to permit persons to whom the Software is
+///  furnished to do so, subject to the following conditions:
+///
+///  The above copyright notice and this permission notice shall be included in
+///  all copies or substantial portions of the Software.
+///
+///  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+///  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+///  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+///  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+///  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+///  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+///  THE SOFTWARE.
+
+#include <random/Encrypter.hpp>
+
+namespace SUNphi
+{
+  namespace Sitmo
+  {
+    /// Class encapsulating the Sitmo random generator
+    DEFINE_SERIALIZABLE_CLASS(Rng)
+    {
+      SERIALIZABLE_SEQUENCE(Sitmo::Key,key);
+      
+      SERIALIZABLE_SEQUENCE(Sitmo::Word,state);
+      
+    public:
+      
+      LIST_SERIALIZABLE_MEMBERS(key,state);
+    };
+  }
+}
+
+#endif
+  
