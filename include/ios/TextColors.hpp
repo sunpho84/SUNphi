113775d8d2f0ba4d04c30c33d66b64aaebdd9b7a
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-05-20 23:50:29 +0200

    Adding styles

--- include/ios/TextColors.hpp
+++ include/ios/TextColors.hpp
@@ -1,15 +1,15 @@
-#ifndef _TEXTCOLORS_HPP
-#define _TEXTCOLORS_HPP
+#ifndef _TEXT_FORMAT_HPP
+#define _TEXT_FORMAT_HPP
 
-/// \file TextColors.hpp
+/// \file TextFormat.hpp
 ///
-/// \brief Define colors to print on the terminal
+/// \brief Define format to print on the terminal
 ///
 /// See https://www.growingwiththeweb.com/2015/05/colours-in-gnome-terminal.html
 
 namespace SUNphi
 {
-  /// First part of the string whcih change color
+  /// First part of the string which change color
   [[ maybe_unused ]]
   static constexpr char TEXT_CHANGE_COLOR_HEAD[]=
     "\x1b[3";
@@ -31,7 +31,27 @@ namespace SUNphi
 			       DEFAULT='9'};
   
   
-  #undef DEFINE_TEXT_COLOR
+  /// First part of the string which change style
+  [[ maybe_unused ]]
+  static constexpr char TEXT_CHANGE_STYLE_HEAD[]=
+    "\x1b[1";
+  
+  /// Last part of the string whcih change style
+  [[ maybe_unused ]]
+  static constexpr char TEXT_CHANGE_STYLE_TAIL[]=
+    "m";
+  
+  /// Identifier of the various supported styles
+  enum class TextStyle : char {RESET='0',
+			       BOLD='1',
+			       DIM='2',
+			       ITALIC='3',
+			       UNDERLINE='4',
+			       BLINK='5',
+			       REVERSE='7',
+			       HIDDEN='8',
+			       STRIKETHROUGH='9'};
+  
 }
 
 #endif
